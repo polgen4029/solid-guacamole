afe825bca00cadd64a1d97d7ef8a535d2c1eb331
polgen4029 <polgen4029@naver.com>
2025-10-21 18:16:17 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -117,6 +117,13 @@ function titleScrollTop () {
     });
 }
 
+    function setVh() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    window.addEventListener('resize', setVh);
+    setVh();
+
 
 { window.addEventListener("scroll" ,() => {
     const currentScrollY = window.pageYOffset
