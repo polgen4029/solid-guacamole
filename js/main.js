fd5275b539fa3509a86bbc95c995459132d07b19
polgen4029 <polgen4029@naver.com>
2025-10-24 17:56:52 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 
 
-function setVh() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-window.addEventListener('load', setVh);
-window.addEventListener('resize', setVh);
-
-
 
 {
     //타이틀 애니메이션
